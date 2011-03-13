d955e7fcd7872c2844fa2cc61acb945121ac2b8c
Mattias Geniar <mattias.geniar@gmail.com>
2011-03-13 01:55:54 +0100

    Cleaned up some code & added more comments.

--- firefox-extension/chrome/content/ff-overlay.js
+++ firefox-extension/chrome/content/ff-overlay.js
@@ -1,21 +1,16 @@
 // First: find all links within the document
 ShortlinkRevealer.attachUrlHooks = function(event) {
-	//alert('call started');
-	/*href_elements = document.getElementsByTagName('a');
-	alert(href.elements.length);
-	for (i = 0; i < href_elements.length; i++) {
-		alert(i);
-	}*/
-	
-	var shortlinkrevealer = document.getElementById("appcontent");   // browser
+	var shortlinkrevealer = document.getElementById("appcontent");   // The browser
 	
     if (shortlinkrevealer) {
+		// Add an event listener for when the entire DOM has loaded (sort of an "on full page load" event)
 		shortlinkrevealer.addEventListener("DOMContentLoaded", ShortlinkRevealer.onPageLoad, true);
 	}
 }
 
 // When the page has loaded, parse all A-elements
 ShortlinkRevealer.onPageLoad = function(aEvent) {
+	// This gets passed via the DOMContentLoadede event handler
     var origdoc = aEvent.originalTarget;
 	
 	// Find all a tags
@@ -32,25 +27,40 @@ ShortlinkRevealer.onMouseOver = function (aEvent) {
 	var doc_href = this.href;	// the href location
 	var doc_name = this.firstChild.nodeValue; // the name of the link
 
-	// Verify the URL
-	var req = new XMLHttpRequest();
-    req.open('GET', 'http://shortlinkrevealer.mattiasgeniar.be/site/?url_check='+ doc_href, true);	
-    req.onreadystatechange = function (aEvt) {				
-		if (req.readyState == 4) {
-			// We've retrieved the state
-			xmlDoc = req.responseXML;
-			resp_httpstatus = xmlDoc.firstChild.childNodes[0].childNodes[0].nodeValue;
-			if (resp_httpstatus != 200) {
-				resp_location = xmlDoc.firstChild.childNodes[1].childNodes[0].nodeValue
+	// This is just a (silly) check, but if a URL has more than X characters
+	// I don't process it, since it's not really a "short" url ;-)
+	// Short URLS are like:
+	// - http://bit.ly/hskAtE
+	// - http://bit.ly/gr8mk4
+	// - http://ncl.uz/gdA0rf
+	// So: "http://" the protocol is 7 chars, the domain usually is 5-7 chars, and the hash is 5-7 chars.
+	alert('URL length: '+ doc_href.length);
+	if (doc_href.length <= 24) {	
+		// Verify the URL
+		var req = new XMLHttpRequest();
+		req.open('GET', 'http://shortlinkrevealer.mattiasgeniar.be/site/?url_check='+ doc_href, true);	
+		req.onreadystatechange = function (aEvt) {				
+			if (req.readyState == 4) {
+				// We've retrieved the state
+				xmlDoc = req.responseXML;
+				resp_httpstatus = xmlDoc.firstChild.childNodes[0].childNodes[0].nodeValue;
 				
-				// Show it
-				displayUrlInfo (doc_name, resp_httpstatus +': '+ resp_location);
+				// Only display something if the HTTP STATUS CODE was not 200 (=OK)
+				if (resp_httpstatus != 200) {
+					resp_location = xmlDoc.firstChild.childNodes[1].childNodes[0].nodeValue
+					
+					// Show it
+					displayUrlInfo (doc_name, resp_httpstatus +': '+ resp_location);
+				}
 			}
-		}
-    };
-	req.send(null);	
+		};
+		
+		// Now fire away the Ajax request
+		req.send(null);	
+	}
 }
 
+// This function just comes directly from the Mozilla Documentation.
 function displayUrlInfo (title, msg) {
   var image = null;
   var win = Components.classes['@mozilla.org/embedcomp/window-watcher;1'].
@@ -60,4 +70,5 @@ function displayUrlInfo (title, msg) {
   win.arguments = [image, title, msg, false, ''];
 }
 
+// Load our own Event Handler! Yay!
 window.addEventListener("load", function (e) { ShortlinkRevealer.attachUrlHooks(e); }, false);
